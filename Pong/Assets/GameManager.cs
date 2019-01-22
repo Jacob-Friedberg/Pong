1adc227aa70007aa1160dff9a6a5c87aadf5deee
scouter238 <jacobfs2009@hotmail.com>
2019-01-22 03:47:50 -0800

    Ball and paddle movment

---
+++ Pong/Assets/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public Paddle paddle;
+    public Ball ball;
+
+    public static Vector2 bottomLeft;
+    public static Vector2 topRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //convert pixel cords into game cords (origin 0,0)
+        bottomLeft = Camera.main.ScreenToWorldPoint(new Vector2(0, 0));
+
+        //convert pixel cords into game cords (origin 0,0)
+        topRight = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width,Screen.height));
+
+        Instantiate(ball);
+        Paddle paddle1 = Instantiate(paddle) as Paddle;
+        Paddle paddle2 = Instantiate(paddle) as Paddle;
+
+        paddle1.Init(true); //Left Paddle   
+        paddle2.Init(false); // right Paddle
+
+    }
+
+}
---
+++ Pong/Assets/Paddle.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField]
+    float speed;
+
+    float height;
+
+    string input;
+    bool isRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        height = transform.localScale.y;
+    }
+
+    public void Init(bool isRightPaddle)
+    {
+        isRight = isRightPaddle;
+        Vector2 pos = Vector2.zero;
+        
+        if(isRightPaddle)
+        {
+            pos = new Vector2(GameManager.topRight.x,0);
+            pos -= Vector2.right * transform.localScale.x;
+
+            input = "PaddleRight";
+        }
+        else
+        {
+            pos = new Vector2(GameManager.bottomLeft.x, 0);
+            pos += Vector2.right * transform.localScale.x;
+
+            input = "PaddleLeft";
+        }
+        //update paddle's position
+        transform.position = pos;
+
+        transform.name = input;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float move = Input.GetAxis(input) * Time.deltaTime * speed;
+        //restrict paddle movement Y
+        if(transform.position.y < GameManager.bottomLeft.y + height/2 && move < 0)
+        {
+            move = 0;
+
+        }
+        //restrict paddle movement X
+        if (transform.position.y > GameManager.topRight.y - height / 2 && move > 0)
+        {
+            move = 0;
+
+        }
+
+        transform.Translate(move * Vector2.up);
+    }
+}
